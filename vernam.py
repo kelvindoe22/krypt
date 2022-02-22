75402c73f1e650022edb3775dae1e463ce911b69
kelvindoe22 <kelvindoe22@gmail.com>
2022-02-22 00:13:00 +0000

    decrypt added

--- vernam.py
+++ vernam.py
@@ -6,11 +6,21 @@ def zip_up(string: str, key: str):
     z = zip(list(string),list(key))
     return list(z)
 
-def make_ascii_lower(tup):
-    return chr((((ord(tup[0]) - 97) + (ord(tup[1]) - 97)) % 26) + 97)
+def make_ascii_lower(tup, encrypt = True):
+    if encrypt:
+        return chr((((ord(tup[0]) - 97) + (ord(tup[1]) - 97)) % 26) + 97)
+    else: # decrypt
+        return chr((((ord(tup[0]) - 97) - (ord(tup[1]) - 97)) % 26) + 97)
 
 def encrypt(message: str, key: str):
     ret_val = ""
     for i in zip_up(message, key):
         ret_val += make_ascii_lower(i)
+    return ret_val
+
+
+def decrypt(cipher: str, key: str):
+    ret_val = ""
+    for i in zip_up(cipher, key):
+        ret_val += make_ascii_lower(i, encrypt=False)
     return ret_val
